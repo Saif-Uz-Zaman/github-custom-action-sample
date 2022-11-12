17e8d82da503f50be5abe48e8ff16e3bcdcbebbe
saif_maruf <saif@swordfishtech.co.uk>
2022-11-12 23:39:32 +0600

    Refactor into async

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import core from "@actions/core";
 import github from "@actions/github";
 
-try {
-  const nameToGreet: string = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}`);
+async function run(): Promise<void> {
+  try {
+    const nameToGreet: string = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}`);
 
-  const time: string = new Date().toTimeString();
-  core.setOutput("time", time);
+    const time: string = new Date().toTimeString();
+    core.setOutput("time", time);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload: string = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  if (error instanceof Error) core.setFailed(error.message);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload: string = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+  }
 }
+
+run()
