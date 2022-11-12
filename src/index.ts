44bf8972298432ee7e2289f0491366af305644af
saif_maruf <saif@swordfishtech.co.uk>
2022-11-12 23:12:25 +0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import core from "@actions/core";
+import github from "@actions/github";
+
+try {
+  const nameToGreet: string = core.getInput("who-to-greet");
+  console.log(`Hello ${nameToGreet}`);
+
+  const time: string = new Date().toTimeString();
+  core.setOutput("time", time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload: string = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  if (error instanceof Error) core.setFailed(error.message);
+}
